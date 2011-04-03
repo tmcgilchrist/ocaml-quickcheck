2807f39feda7b95fdca4e196bd78f1992d954e30
Roman Sokolov <sokolov.r.v@gmail.com>
2011-04-03 12:30:58 +1200

    more tests

--- tests/test.ml
+++ tests/test.ml
@@ -4,18 +4,39 @@ open Arbitrary
 open Show
 open Testable
 
-let prop_revrev : 'a list -> bool =
-  fun xs -> List.rev (List.rev xs) = xs
+let prop_revrev xs =
+  List.rev (List.rev xs) = xs
+
+let prop_mem xs = match xs with
+  | [] -> true
+  | (x::_) -> List.mem x xs
+
+let prop_str_copy s =
+  s = (String.copy s)
 
 (* for generating random int lists *)
-module AL = Arbitrary_list(Arbitrary_int) ;;
+module AL = Arbitrary_list(Arbitrary_int)
 (* for printing out int lists *)
-module SL = PShow_list(PShow_int) ;;
+module SL = PShow_list(PShow_int)
 (* for being able to test (int list -> bool) *)
 module Testable_list_to_bool =
   Testable_fun
     (AL)
     (SL)
-    (Testable_bool) ;;
-module C = Check(Testable_list_to_bool)
-let () = C.quickCheck prop_revrev
+    (Testable_bool)
+module CL = Check(Testable_list_to_bool)
+
+module Testable_str_to_bool =
+  Testable_fun
+    (Arbitrary_string)
+    (PShow_string)
+    (Testable_bool)
+
+module CS = Check(Testable_str_to_bool)
+
+
+let () =
+  CL.quickCheck prop_revrev;
+  CL.quickCheck prop_mem;
+  CS.quickCheck prop_str_copy
+
